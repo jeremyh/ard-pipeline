Fixed issue regarding integer overflow with numexpression in determining contiguity.

image_processor/pqa/calc_pqa_masks/contiguity_masking.py
@@ -1,7 +1,6 @@
 import logging
 import os
 
-import numexpr
 import numpy as np
 from IDL_functions import histogram
 from scipy import ndimage
@@ -129,11 +128,12 @@ def process(subprocess_list=[], resume=False):
             return None
 
         assert type(image_stack[0]) == np.ndarray, "Input is not valid"
+        assert len(image_stack.shape) == 3, "Input array must contain 3 dimensions!"
 
         logger.debug("Determining pixel contiguity")
         # Create mask array with True for all pixels which are non-zero in all bands
-        # mask = image_stack.all(0)
-        mask = numexpr.evaluate("prod(image_stack, 0)") != 0
+        mask = image_stack.all(0)
+        # mask = numexpr.evaluate('prod(image_stack, 0)') != 0 # ***This has the potential to overflow, roll back to array.all(0)***
 
         # The following is only valid for Landsat 5 images
         if satellite.TAG == "LS5":