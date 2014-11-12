834d6e1c5d1abdbab43d67b501bf2f27f78d6c0d
Steven Ring <steven.ring@ga.gov.au>
2014-11-12 17:07:42 +1100

    Fix stuff up

--- workflow/pqa/pq.py
+++ workflow/pqa/pq.py
@@ -3,11 +3,17 @@
 import logging
 import os
 
+# TODO: remove soon
+from glob import glob
+
 import luigi
 from acca_cloud_masking import calc_acca_cloud_mask
+from cloud_shadow_masking import Cloud_Shadow
 from constants import PQAConstants
 from contiguity_masking import setContiguityBit
 from EOtools.DatasetDrivers import SceneDataset
+from fmask_cloud_masking_wrapper import FMaskCloudMask
+from GriddedGeoBox import GriddedGeoBox
 from land_sea_masking import setLandSeaBit
 from memuseFilter import MemuseFilter
 from pqa_result import PQAResult
@@ -38,9 +44,18 @@ class PixelQualityTask(luigi.Task):
         # read L1T data
         logging.debug("Creating L1T SceneDataset")
         l1t_sd = SceneDataset(self.l1t_path)
+        logging.debug(f"Satellite is {l1t_sd.satellite.TAG}")
         logging.debug("Reading L1T bands")
         l1t_data = l1t_sd.ReadAsArray()
         logging.debug("l1t_data shape=%s" % (str(l1t_data.shape)))
+
+        # get the GriddedGeoBox for this dataset
+        # GriddedGeoBox encapsulates the bounding box, the pixel
+        # grid and the Co-ordinate reference system
+        # TODO: rework this to eliminate the reliance on SceneDataset
+
+        geoBox = GriddedGeoBox.from_dataset(l1t_sd._root_dataset)
+        logging.debug(str(geoBox))
 
         # read NBAR data
         logging.debug("Creating NBAR SceneDataset")
@@ -57,7 +72,7 @@ class PixelQualityTask(luigi.Task):
 
         # the PQAResult object for this run
 
-        pqaResult = PQAResult(l1t_data[0].shape)
+        pqaResult = PQAResult(l1t_data[0].shape, geoBox)
 
         # Saturation
 
@@ -120,6 +135,8 @@ class PixelQualityTask(luigi.Task):
         if pq_const.run_cloud:
             mask = None
             aux_data = {}  # for collecting result metadata
+
+            # TODO: pass in scene metadata via Dale's new MTL reader
             mtl = glob(os.path.join(l1t_sd.pathname, "scene01/*_MTL.txt"))[
                 0
             ]  # Crude but effective
@@ -131,7 +148,7 @@ class PixelQualityTask(luigi.Task):
             )
 
             # set the result
-            pqaResult.set_mask(mask, pq_const.fmsk)
+            pqaResult.set_mask(mask, pq_const.fmask)
             pqaResult.add_to_aux_data(aux_data)
         else:
             logging.warning(
@@ -139,6 +156,114 @@ class PixelQualityTask(luigi.Task):
                     sensor
                 )
             )
+
+        logging.debug("done calculating fmask cloud mask")
+
+        # parameters for cloud shadow masks
+
+        contiguity_mask = pqaResult.get_mask(pq_const.contiguity)
+        land_sea_mask = pqaResult.get_mask(pq_const.land_sea)
+
+        # acca cloud shadow
+
+        logging.debug("calculating ACCA cloud shadow mask")
+        if pq_const.run_cloud_shadow:  # TM/ETM/OLI_TIRS
+            mask = None
+            aux_data = {}  # for collecting result metadata
+
+            cloud_mask = pqaResult.get_mask(pq_const.acca)
+            if pq_const.oli_tirs:
+                mask = Cloud_Shadow(
+                    nbar_data[1:, :, :],
+                    kelvin_band,
+                    cloud_mask,
+                    l1t_sd,
+                    pq_const,
+                    land_sea_mask=land_sea_mask,
+                    contiguity_mask=contiguity_mask,
+                    cloud_algorithm="ACCA",
+                    growregion=True,
+                    aux_data=aux_data,
+                )
+            else:  # TM or ETM
+                mask = Cloud_Shadow(
+                    nbar_data,
+                    kelvin_band,
+                    cloud_mask,
+                    l1t_sd,
+                    pq_const,
+                    land_sea_mask=land_sea_mask,
+                    contiguity_mask=contiguity_mask,
+                    cloud_algorithm="ACCA",
+                    growregion=True,
+                    aux_data=aux_data,
+                )
+
+            pqaResult.set_mask(mask, pq_const.acca_shadow)
+            pqaResult.add_to_aux_data(aux_data)
+
+        else:  # OLI/TIRS only
+            logger.warning(
+                "Cloud Shadow Algorithm Not Run! {} sensor not configured for the cloud shadow algorithm.".format(
+                    sensor
+                )
+            )
+
+        logging.debug("done calculating ACCA cloud shadow mask")
+
+        # FMASK cloud shadow
+
+        logging.debug("calculating FMASK cloud shadow mask")
+        if pq_const.run_cloud_shadow:  # TM/ETM/OLI_TIRS
+            mask = None
+            aux_data = {}  # for collecting result metadata
+
+            cloud_mask = pqaResult.get_mask(pq_const.fmask)
+            if pq_const.oli_tirs:
+                mask = Cloud_Shadow(
+                    nbar_data[1:, :, :],
+                    kelvin_band,
+                    cloud_mask,
+                    l1t_sd,
+                    pq_const,
+                    land_sea_mask=land_sea_mask,
+                    contiguity_mask=contiguity_mask,
+                    cloud_algorithm="FMASK",
+                    growregion=True,
+                    aux_data=aux_data,
+                )
+            else:  # TM or ETM
+                mask = Cloud_Shadow(
+                    nbar_data,
+                    kelvin_band,
+                    cloud_mask,
+                    l1t_sd,
+                    pq_const,
+                    land_sea_mask=land_sea_mask,
+                    contiguity_mask=contiguity_mask,
+                    cloud_algorithm="FMASK",
+                    growregion=True,
+                    aux_data=aux_data,
+                )
+
+            pqaResult.set_mask(mask, pq_const.fmask_shadow)
+            pqaResult.add_to_aux_data(aux_data)
+
+        else:  # OLI/TIRS only
+            logger.warning(
+                "Cloud Shadow Algorithm Not Run! {} sensor not configured for the cloud shadow algorithm.".format(
+                    sensor
+                )
+            )
+
+        logging.debug("done calculating FMASK cloud shadow mask")
+
+        # write PQA file as output
+
+        logging.debug("saving PQA result GeoTiff")
+        pqa_output_path = os.path.join(self.pq_path, "pqa.tif")
+        pqaResult.save_as_tiff(pqa_output_path)
+        logging.debug("done saving PQA result GeoTiff")
 
 
 class PQDataset(luigi.Target):
