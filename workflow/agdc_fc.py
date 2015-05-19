1c4fa208cc74687a51b665d8cab2291d8cd6e576
Josh Sixsmith <joshua.sixsmith@ga.gov.au>
2015-05-19 17:14:54 +1000

    Changed to use normal luigi; local scheduler.

--- workflow/agdc_fc.py
+++ workflow/agdc_fc.py
@@ -1,81 +1,24 @@
 #!/bin/env python
 
-import luigi.contrib.mpi as mpi
 
-import luigi
 import os
-from os.path import join as pjoin, dirname, exists, splitext, basename
-import gaip
-import cPickle as pickle
+from os.path import basename, dirname, exists, splitext
+from os.path import join as pjoin
 
-from datacube.api.model import DatasetType, Satellite
-from datacube.api.query import list_tiles_as_list
-from datacube.config import Config
-from datetime import date
+import cPickle as pickle
+import luigi
+from datacube.api.model import DatasetType
 from EOtools.DatasetDrivers.stacked_dataset import StackedDataset
 
+import gaip
+
 CONFIG = luigi.configuration.get_config()
-CONFIG.add_config_path(pjoin(dirname(__file__), 'fc.cfg'))
-
-class TileQuery(luigi.Task):
-    """
-    Queries the database based on the input parameters.
-    """
-    out_path = luigi.Parameter()
-
-    config = Config()
-    ds_type = DatasetType.ARG25
-
-    def requires(self):
-        return []
-
-    def output(self):
-        out_file = pjoin(self.out_path, CONFIG.get('agdc', 'query_result'))
-        return luigi.LocalTarget(out_file)
-
-    def run(self):
-        print "Executing DB query!"
-
-        if not os.path.exists(self.out_path):
-            os.makedirs(self.out_path)
-
-        satellites = CONFIG.get('agdc', 'satellites')
-        satellites = [Satellite(i) for i in satellites.split(',')]
-
-        min_date = CONFIG.get('agdc', 'min_date')
-        min_date = [int(i) for i in min_date.split('_')]
-        min_date = date(min_date[0], min_date[1], min_date[2])
-
-        max_date = CONFIG.get('agdc', 'max_date')
-        max_date = [int(i) for i in max_date.split('_')]
-        max_date = date(max_date[0], max_date[1], max_date[2])
-
-        cell_xmin = int(CONFIG.get('agdc', 'cell_xmin'))
-        cell_ymin = int(CONFIG.get('agdc', 'cell_ymin'))
-        cell_xmax = int(CONFIG.get('agdc', 'cell_xmax'))
-        cell_ymax = int(CONFIG.get('agdc', 'cell_ymax'))
-
-        xcells = range(cell_xmin, cell_xmax, 1)
-        ycells = range(cell_ymin, cell_ymax, 1)
-
-        tiles = list_tiles_as_list(x=xcells, y=ycells, acq_min=min_date,
-                                   acq_max=max_date,
-                                   satellites=satellites,
-                                   datasets=self.ds_type,
-                                   database=self.config.get_db_database(),
-                                   user=self.config.get_db_username(),
-                                   password=self.config.get_db_password(),
-                                   host=self.config.get_db_host(),
-                                   port=self.config.get_db_port())
-
-        with self.output().open('w') as outf:
-            pickle.dump(tiles, outf)
+CONFIG.add_config_path(pjoin(dirname(__file__), "fc.cfg"))
 
 
 class FractionalCoverTask(luigi.Task):
-    """
-    Computes fractional cover for a given input.
-    """
+    """Computes fractional cover for a given input."""
+
     fname = luigi.Parameter()
     out_dir = luigi.Parameter()
 
@@ -86,10 +29,10 @@ class FractionalCoverTask(luigi.Task):
         base_name = splitext(basename(self.fname))[0]
 
         # Comment out the next line if QDERM test is to run
-        base_name = base_name.replace('NBAR', 'FC') + '.tif'
+        base_name = base_name.replace("NBAR", "FC") + ".tif"
 
         # QDERM TEST
-        #base_name = base_name.replace('dbgm4', 'FC') + '_byte.tif'
+        # base_name = base_name.replace('dbgm4', 'FC') + '_byte.tif'
 
         out_fname = pjoin(self.out_dir, base_name)
         return luigi.LocalTarget(out_fname)
@@ -98,8 +41,8 @@ class FractionalCoverTask(luigi.Task):
         out_fname = self.output().path
 
         # Get the processing tile sizes
-        x_tile = int(CONFIG.get('work', 'x_tile_size'))
-        y_tile = int(CONFIG.get('work', 'y_tile_size'))
+        x_tile = int(CONFIG.get("work", "x_tile_size"))
+        y_tile = int(CONFIG.get("work", "y_tile_size"))
         x_tile = None if x_tile <= 0 else x_tile
         y_tile = None if y_tile <= 0 else y_tile
 
@@ -109,25 +52,26 @@ class FractionalCoverTask(luigi.Task):
 
 
 class ProcessFC(luigi.Task):
-    """
-    Creates FractionalCoverTask's based on the results determined from
+    """Creates FractionalCoverTask's based on the results determined from
     the database query.
     """
+
     out_path = luigi.Parameter()
+    idx1 = luigi.IntParameter()
+    idx2 = luigi.IntParameter()
 
     ds_type = DatasetType.ARG25
 
     def requires(self):
-        query_result = pjoin(self.out_path, CONFIG.get('agdc', 'query_result'))
-        with open(query_result, 'r') as f:
+        query_result = pjoin(self.out_path, CONFIG.get("agdc", "query_result"))
+        with open(query_result) as f:
             query = pickle.load(f)
 
         tasks = []
-        for ds in query:
+        for ds in query[self.idx1 : self.idx2]:
             fname = ds.datasets[self.ds_type].path
 
-            cell_out_dir = '{cell_x}_{cell_y}'.format(cell_x=ds.x,
-                                                      cell_y=ds.y)
+            cell_out_dir = f"{ds.x}_{ds.y}"
 
             out_dir = pjoin(self.out_path, cell_out_dir)
             if not exists(out_dir):
@@ -138,28 +82,59 @@ class ProcessFC(luigi.Task):
         return tasks
 
     def output(self):
-        out_fname = pjoin(self.out_path, 'ProcessFC.completed')
+        out_fname = pjoin(self.out_path, "ProcessFC.completed")
         return luigi.LocalTarget(out_fname)
 
     def run(self):
-        with self.output().open('w') as outf:
-            outf.write('Completed')
+        with self.output().open("w") as outf:
+            outf.write("Completed")
 
 
-if __name__ == '__main__':
-    out_dir = CONFIG.get('agdc', 'output_directory')
+if __name__ == "__main__":
+    # Setup command-line arguments
+    desc = "Processes fractional cover.."
+    hlp = (
+        "The tile/chunk index to retieve from the tiles list. "
+        "(Needs to have been previously computed to a file named chunks.pkl"
+    )
+    parser = argparse.ArgumentParser(description=desc)
+    parser.add_argument("--tile", type=int, help=hlp)
 
-    tasks = [TileQuery(out_dir)]
-    mpi.run(tasks)
-    tasks = [ProcessFC(out_dir)]
-    mpi.run(tasks)
+    parsed_args = parser.parse_args()
+    tile_idx = parsed_args.tile
 
+    # setup logging
+    log_dir = CONFIG.get("work", "logs_directory")
+    if not exists(log_dir):
+        os.makedirs(log_dir)
 
+    logfile = "{log_path}/run_wc_{uname}_{pid}.log"
+    logfile = logfile.format(log_path=log_dir, uname=os.uname()[1], pid=os.getpid())
+    logging_level = logging.INFO
+    logging.basicConfig(
+        filename=logfile,
+        level=logging_level,
+        format=("%(asctime)s: [%(name)s] (%(levelname)s) " "%(message)s "),
+    )
+
+    out_dir = CONFIG.get("agdc", "output_directory")
+
+    # Get the chunks list (each node will process a chunk determined by the
+    # chunks.pkl, and the index to the chunk is determined at run time
+    chunks_list_fname = pjoin(out_dir, CONFIG.get("agdc", "chunks_list"))
+    with open(chunks_list_fname) as f:
+        chunks = pickle.load(f)
+
+    # Initialise the job to luigi
+    chunk = chunks[tile_idx]
+    tasks = [ProcessFC(out_dir, chunk[0], chunk[1])]
+    luigi.build(tasks, local_scheduler=True, workers=16)
+    luigi.run()
 
     # *********************** QDERM TEST ******************************
     # For the QDERM test to run, a few lines need to be commented and
     # uncommented both here in agdc_fc.py and fc_utils.py
 
-    #task = [FractionalCoverTask('/g/data1/v10/testing_ground/jps547/FC/QDERM_test_data/l5tmre_p095r084_20090411/l5tmre_p095r084_20090411_dbgm4.img',
+    # task = [FractionalCoverTask('/g/data1/v10/testing_ground/jps547/FC/QDERM_test_data/l5tmre_p095r084_20090411/l5tmre_p095r084_20090411_dbgm4.img',
     #                            '/g/data1/v10/testing_ground/jps547/FC/QDERM_test_data/l5tmre_p095r084_20090411')]
-    #mpi.run(task)
+    # mpi.run(task)
