30d9ade71e2ced624f5082586e19becb0e33e713
Josh Sixsmith <joshua.sixsmith@ga.gov.au>
2016-06-08 17:25:42 +1000

    Updated get_version.

--- gaip/version.py
+++ gaip/version.py
@@ -19,52 +19,58 @@ This script is derived from https://github.com/Changaco/version.py
 from __future__ import absolute_import, print_function
 
 import re
+import os
 from os.path import dirname, isdir, join, exists
 from subprocess import CalledProcessError, check_output
 
 import pkg_resources
 
-PREFIX = "gaip-"
+PREFIX = 'gaip-'
 
-GIT_TAG_PATTERN = re.compile(r"\btag: %s([0-9][^,]*)\b" % PREFIX)
-VERSION_PATTERN = re.compile(r"^Version: (.+)$", re.M)
-NUMERIC = re.compile(r"^\d+$")
+GIT_TAG_PATTERN = re.compile(r'\btag: %s([0-9][^,]*)\b' % PREFIX)
+VERSION_PATTERN = re.compile(r'^Version: (.+)$', re.M)
+NUMERIC = re.compile(r'^\d+$')
 
-GIT_ARCHIVE_REF_NAMES = "$Format:%D$"
-GIT_ARCHIVE_COMMIT_HASH = "$Format:%h$"
+GIT_ARCHIVE_REF_NAMES = '$Format:%D$'
+GIT_ARCHIVE_COMMIT_HASH = '$Format:%h$'
 
 
 def get_version():
     package_dir = dirname(dirname(__file__))
-    git_dir = join(package_dir, ".git")
+    git_dir = join(package_dir, '.git')
 
     if isdir(git_dir):
         # Ask git for an annotated version number
         # (eg. "gaip-0.0.0-651-gcf335a9-dirty")
         cmd = [
-            "git",
-            "--git-dir",
-            git_dir,
-            "describe",
-            "--tags",
-            "--match",
-            "[0-9]*",
-            "--dirty",
+            'git',
+            'describe', '--tags', '--match', '[0-9]*', '--dirty'
         ]
-        try:
-            git_version = check_output(cmd).decode().strip()
-        except CalledProcessError:
-            raise RuntimeError("Unable to get version number from git tags")
-        components = git_version.split("-")
-        version = components.pop(0)
+    with remember_cwd():
+        os.chdir(package_dir)
+            try:
+                git_version = check_output(cmd).decode().strip()
+            except CalledProcessError:
+                raise RuntimeError('Unable to get version number from git tags')
 
-        # Any other suffixes imply this is not a release: Append an internal build number
-        if components:
-            # <commit count>.<git hash>.<whether the working tree is dirty>
-            version += "+" + ".".join(components)
+    components = git_version.split('-')
+    version = components.pop(0)
+
+    # Any other suffixes imply this is not a release: Append an internal build number
+    if components:
+        # <commit count>.<git hash>.<whether the working tree is dirty>
+        version += '+' + '.'.join(components)
 
     return version
 
 
-if __name__ == "__main__":
+def remember_cwd():
+    current_dir = os.getcwd()
+    try:
+        yield
+    finally:
+        os.chdir(current_dir)
+
+
+if __name__ == '__main__':
     print(get_version())
