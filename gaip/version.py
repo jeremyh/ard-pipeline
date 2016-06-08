2bad4f6a89b57dee4b515dd097c43932caa6e50e
Josh Sixsmith <joshua.sixsmith@ga.gov.au>
2016-06-09 09:50:23 +1000

    Minor fix to get_version.

--- gaip/version.py
+++ gaip/version.py
@@ -25,41 +25,38 @@ from subprocess import CalledProcessError, check_output
 
 import pkg_resources
 
-PREFIX = 'gaip-'
+PREFIX = "gaip-"
 
-GIT_TAG_PATTERN = re.compile(r'\btag: %s([0-9][^,]*)\b' % PREFIX)
-VERSION_PATTERN = re.compile(r'^Version: (.+)$', re.M)
-NUMERIC = re.compile(r'^\d+$')
+GIT_TAG_PATTERN = re.compile(r"\btag: %s([0-9][^,]*)\b" % PREFIX)
+VERSION_PATTERN = re.compile(r"^Version: (.+)$", re.M)
+NUMERIC = re.compile(r"^\d+$")
 
-GIT_ARCHIVE_REF_NAMES = '$Format:%D$'
-GIT_ARCHIVE_COMMIT_HASH = '$Format:%h$'
+GIT_ARCHIVE_REF_NAMES = "$Format:%D$"
+GIT_ARCHIVE_COMMIT_HASH = "$Format:%h$"
 
 
 def get_version():
     package_dir = dirname(dirname(__file__))
-    git_dir = join(package_dir, '.git')
+    git_dir = join(package_dir, ".git")
 
     if isdir(git_dir):
         # Ask git for an annotated version number
         # (eg. "gaip-0.0.0-651-gcf335a9-dirty")
-        cmd = [
-            'git',
-            'describe', '--tags', '--match', '[0-9]*', '--dirty'
-        ]
-    with remember_cwd():
-        os.chdir(package_dir)
+        cmd = ["git", "describe", "--tags", "--match", "[0-9]*", "--dirty"]
+        with remember_cwd():
+            os.chdir(package_dir)
             try:
                 git_version = check_output(cmd).decode().strip()
             except CalledProcessError:
-                raise RuntimeError('Unable to get version number from git tags')
+                raise RuntimeError("Unable to get version number from git tags")
 
-    components = git_version.split('-')
-    version = components.pop(0)
+            components = git_version.split("-")
+            version = components.pop(0)
 
-    # Any other suffixes imply this is not a release: Append an internal build number
-    if components:
-        # <commit count>.<git hash>.<whether the working tree is dirty>
-        version += '+' + '.'.join(components)
+            # Any other suffixes imply this is not a release: Append an internal build number
+            if components:
+                # <commit count>.<git hash>.<whether the working tree is dirty>
+                version += "+" + ".".join(components)
 
     return version
 
@@ -72,5 +69,5 @@ def remember_cwd():
         os.chdir(current_dir)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     print(get_version())
