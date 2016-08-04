7a8fafb66e5fa4b5207b23daa321ca05182b14c8
Josh Sixsmith <josh.sixsmith@gmail.com>
2016-08-04 22:51:53 +1000

    Added S2A sensor configuration.

--- gaip/acquisition.py
+++ gaip/acquisition.py
@@ -835,8 +835,8 @@ class Sentinel2aAcquisition(Acquisition):
     # TODO: update nbar_constants for new sensor
     @property
     def spacecraft_id(self):
-        raise NotImplementedError
+        return "Sentinel2A"
 
     @property
     def sensor_id(self):
-        raise NotImplementedError
+        return "MSI"
