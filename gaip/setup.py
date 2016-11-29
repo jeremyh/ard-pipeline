52876471090429d6f39c39410cdc6415b6940272
Josh Sixsmith <joshua.sixsmith@ga.gov.au>
2016-11-29 11:27:20 +1100

    Updated build for start_end_coordinates.f90.

--- gaip/setup.py
+++ gaip/setup.py
@@ -63,6 +63,6 @@ setup(
             ],
         ),
         Extension(name="_interpolation", sources=["bilinear_interpolation.f90"]),
-        Extension(name="_boxline", sources=["box_line_coordinates.f90"]),
+        Extension(name="_start_end_coordinates", sources=["start_end_coordinates.f90"]),
     ],
 )
