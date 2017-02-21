eda34b5228ba57a111e95511510d9bb68ce44723
Josh Sixsmith <joshua.sixsmith@ga.gov.au>
2017-02-21 17:34:03 +1100

    Minor fix to numpy shape call

--- gaip/calculate_shadow_masks.py
+++ gaip/calculate_shadow_masks.py
@@ -1,56 +1,66 @@
 #!/usr/bin/env python
 
-"""
-Functions for calculating cast shadow from both the sun and satellite
----------------------------------------------------------------------
+"""Functions for calculating cast shadow from both the sun and satellite
+---------------------------------------------------------------------.
 
 as source directions, as well as self shadow masks.
 ---------------------------------------------------
 """
 
-import numpy
 import h5py
-
+import numpy as np
 from eotools import tiling
-from gaip import GriddedGeoBox
-from gaip import ImageMargins
-from gaip import setup_spheroid
-from gaip import run_castshadow
-from gaip import cast_shadow_main
-from gaip import dataset_compression_kwargs
-from gaip import attach_image_attributes
+
+from gaip import (
+    GriddedGeoBox,
+    ImageMargins,
+    attach_image_attributes,
+    cast_shadow_main,
+    dataset_compression_kwargs,
+    setup_spheroid,
+)
 
 
-def _self_shadow(incident_angles_fname, exiting_angles_fname, out_fname,
-                 compression='lzf', x_tile=None, y_tile=None):
-    """
-    A private wrapper for dealing with the internal custom workings of the
+def _self_shadow(
+    incident_angles_fname,
+    exiting_angles_fname,
+    out_fname,
+    compression="lzf",
+    x_tile=None,
+    y_tile=None,
+):
+    """A private wrapper for dealing with the internal custom workings of the
     NBAR workflow.
     """
-    with h5py.File(incident_angles_fname, 'r') as inci_angles,\
-        h5py.File(exiting_angles_fname, 'r') as exit_angles:
-
-        inci_dset = inci_angles['incident']
-        exit_dset = exit_angles['exiting']
+    with h5py.File(incident_angles_fname, "r") as inci_angles, h5py.File(
+        exiting_angles_fname, "r"
+    ) as exit_angles:
+        inci_dset = inci_angles["incident"]
+        exit_dset = exit_angles["exiting"]
 
         shape = inci_dset.shape
-        transform = inci_dset.attrs['geotransform']
+        transform = inci_dset.attrs["geotransform"]
         origin = (transform[0], transform[3])
-        crs = inci_dset.attrs['crs_wkt']
+        crs = inci_dset.attrs["crs_wkt"]
         pixelsize = (abs(transform[1]), abs(transform[5]))
         geobox = GriddedGeoBox(shape, origin, pixelsize, crs)
 
-        fid = self_shadow(inci_dset, exit_dset, geobox, out_fname, x_tile,
-                          y_tile)
+        fid = self_shadow(inci_dset, exit_dset, geobox, out_fname, x_tile, y_tile)
 
     fid.close()
     return
 
 
-def self_shadow(incident_dataset, exiting_dataset, geobox, out_fname,
-                compression='lzf', x_tile=None, y_tile=None):
-    """
-    Computes the self shadow mask.
+def self_shadow(
+    incident_dataset,
+    exiting_dataset,
+    geobox,
+    out_fname,
+    compression="lzf",
+    x_tile=None,
+    y_tile=None,
+):
+    """Computes the self shadow mask.
 
     :param incident_dataset:
         A `NumPy` or `NumPy` like dataset that allows indexing
@@ -99,25 +109,27 @@ def self_shadow(incident_dataset, exiting_dataset, geobox, out_fname,
     """
     # Initialise the output files
     if out_fname is None:
-        fid = h5py.File('self-shadow.h5', driver='core',
-                        backing_store=False)
+        fid = h5py.File("self-shadow.h5", driver="core", backing_store=False)
     else:
-        fid = h5py.File(out_fname, 'w')
+        fid = h5py.File(out_fname, "w")
 
-    kwargs = dataset_compression_kwargs(compression=compression,
-                                        chunks=(1, geobox.x_size()))
+    kwargs = dataset_compression_kwargs(
+        compression=compression, chunks=(1, geobox.x_size())
+    )
     cols, rows = geobox.get_shape_xy()
-    kwargs['shape'] = (rows, cols)
-    kwargs['dtype'] = 'bool'
+    kwargs["shape"] = (rows, cols)
+    kwargs["dtype"] = "bool"
 
     # output dataset
-    out_dset = fid.create_dataset('self-shadow', **kwargs)
+    out_dset = fid.create_dataset("self-shadow", **kwargs)
 
     # attach some attributes to the image datasets
-    attrs = {'crs_wkt': geobox.crs.ExportToWkt(),
-             'geotransform': geobox.affine.to_gdal()}
+    attrs = {
+        "crs_wkt": geobox.crs.ExportToWkt(),
+        "geotransform": geobox.affine.to_gdal(),
+    }
     desc = "Self shadow mask derived using the incident and exiting angles."
-    attrs['Description'] = desc
+    attrs["Description"] = desc
     attach_image_attributes(out_dset, attrs)
 
     # Initialise the tiling scheme for processing
@@ -132,16 +144,16 @@ def self_shadow(incident_dataset, exiting_dataset, geobox, out_fname,
         # Row and column start locations
         ystart, yend = tile[0]
         xstart, xend = tile[1]
-        idx = (slice(ystart, yend, slice(xstart, xend)))
+        idx = slice(ystart, yend, slice(xstart, xend))
 
         # Read the data for the current tile
-        inc = numpy.radians(incident_dataset[idx])
-        exi = numpy.radians(exiting_dataset[idx])
+        inc = np.radians(incident_dataset[idx])
+        exi = np.radians(exiting_dataset[idx])
 
         # Process the tile
-        mask = numpy.ones(inc.shape, xsize), dtype='uint8')
-        mask[numpy.cos(inc) <= 0.0] = 0
-        mask[numpy.cos(exi) <= 0.0] = 0
+        mask = np.ones(inc.shape, dtype="uint8")
+        mask[np.cos(inc) <= 0.0] = 0
+        mask[np.cos(exi) <= 0.0] = 0
 
         # Write the current tile to disk
         out_dset[idx] = mask
@@ -151,10 +163,7 @@ def self_shadow(incident_dataset, exiting_dataset, geobox, out_fname,
 
 
 class FortranError(Exception):
-
-    """
-    Base class for errors thrown from the Fortran code used in this module.
-    """
+    """Base class for errors thrown from the Fortran code used in this module."""
 
     def __init__(self, function_name, code, msg):
         self.function_name = function_name
@@ -162,110 +171,104 @@ class FortranError(Exception):
         self.msg = msg or "Unknown error"
 
     def __str__(self):
-        """
-        Return a string representation of this Error.
-        """
+        """Return a string representation of this Error."""
         err = "Error in Fotran code {0} (code {1}): {2}"
         err = err.format(self.function_name, self.code, self.msg)
         return err
 
 
 class CastShadowError(FortranError):
-
-    """
-    Class that deals with errors from :py:func:`run_castshadow`.
-    """
+    """Class that deals with errors from :py:func:`run_castshadow`."""
 
     def __init__(self, code):
-        super(CastShadowError,
-              self).__init__("cast_shadow_main",
-                             code,
-                             CastShadowError.get_error_message(code))
+        super().__init__(
+            "cast_shadow_main", code, CastShadowError.get_error_message(code)
+        )
 
     @staticmethod
     def get_error_message(code):
-        """
-        Generate an error message for a specific code. It is OK for this have
+        """Generate an error message for a specific code. It is OK for this have
         non-returning control paths, as this will results in ``None``, which
         is handled in the super class.
         """
+
         def tmpt(d, n):
             """Generate message."""
-            err = "attempt to access invalid {0} of {1}".format(d, n)
+            err = f"attempt to access invalid {d} of {n}"
             return err
 
         if code == 20:
-            return tmpt('x', 'dem')
+            return tmpt("x", "dem")
         if code == 21:
-            return tmpt('x', 'dem_data')
+            return tmpt("x", "dem_data")
         if code == 22:
-            return tmpt('x', 'solar and sazi')
+            return tmpt("x", "solar and sazi")
         if code == 23:
-            return tmpt('x', 'solar_data')
+            return tmpt("x", "solar_data")
         if code == 24:
-            return tmpt('x', 'a')
+            return tmpt("x", "a")
         if code == 25:
-            return tmpt('y', 'dem_data')
+            return tmpt("y", "dem_data")
         if code == 26:
-            return tmpt('y', 'a')
+            return tmpt("y", "a")
         if code == 27:
-            return tmpt('x', 'mask_all')
+            return tmpt("x", "mask_all")
         if code == 28:
-            return tmpt('y', 'mask_all')
+            return tmpt("y", "mask_all")
         if code == 29:
-            return tmpt('x', 'mask')
+            return tmpt("x", "mask")
         if code == 30:
-            return tmpt('y', 'mask')
+            return tmpt("y", "mask")
         if code == 31:
-            return tmpt('X', 'dem and a')
+            return tmpt("X", "dem and a")
         if code == 32:
-            return tmpt('y', 'a')
+            return tmpt("y", "a")
         if code == 33:
-            return tmpt('y', 'dem')
+            return tmpt("y", "dem")
         if code == 34:
-            return tmpt('x', 'mask_all')
+            return tmpt("x", "mask_all")
         if code == 35:
-            return tmpt('x', 'mask')
+            return tmpt("x", "mask")
         if code == 36:
-            return tmpt('y', 'mask_all')
+            return tmpt("y", "mask_all")
         if code == 37:
-            return tmpt('y', 'mask')
+            return tmpt("y", "mask")
         if code == 38:
-            return tmpt('x', 'dem')
+            return tmpt("x", "dem")
         if code == 39:
-            return tmpt('x', 'dem_data')
+            return tmpt("x", "dem_data")
         if code == 40:
-            return tmpt('x', 'solar')
+            return tmpt("x", "solar")
         if code == 41:
-            return tmpt('x', 'solar_data')
+            return tmpt("x", "solar_data")
         if code == 42:
-            return tmpt('x', 'a and dem')
+            return tmpt("x", "a and dem")
         if code == 43:
-            return tmpt('y', 'a')
+            return tmpt("y", "a")
         if code == 44:
-            return tmpt('y', 'dem')
+            return tmpt("y", "dem")
         if code == 45:
-            return tmpt('x', 'mask_all')
+            return tmpt("x", "mask_all")
         if code == 46:
-            return tmpt('x', 'mask')
+            return tmpt("x", "mask")
         if code == 47:
-            return tmpt('y', 'mask_alll')
+            return tmpt("y", "mask_alll")
         if code == 48:
-            return tmpt('y', 'mask')
+            return tmpt("y", "mask")
         if code == 49:
-            return tmpt('x', 'a and dem')
+            return tmpt("x", "a and dem")
         if code == 50:
-            return tmpt('y', 'a')
+            return tmpt("y", "a")
         if code == 51:
-            return tmpt('y', 'dem')
+            return tmpt("y", "dem")
         if code == 52:
-            return tmpt('x', 'mask_all')
+            return tmpt("x", "mask_all")
         if code == 53:
-            return tmpt('x', 'mask')
+            return tmpt("x", "mask")
         if code == 54:
-            return tmpt('y', 'mask_all')
+            return tmpt("y", "mask_all")
         if code == 55:
-            return tmpt('y', 'mask')
+            return tmpt("y", "mask")
         if code == 61:
             return "azimuth case not possible - phi_sun must be in 0 to 360 deg"
         if code == 62:
@@ -282,42 +285,63 @@ class CastShadowError(FortranError):
             return "matrix A does not have sufficient x margin"
 
 
-def _calculate_cast_shadow(acquisition, dsm_fname, margins, block_height,
-                           block_width, satellite_solar_angles_fname,
-                           out_fname, compression='lzf', solar_source=True):
-    """
-    A private wrapper for dealing with the internal custom workings of the
+def _calculate_cast_shadow(
+    acquisition,
+    dsm_fname,
+    margins,
+    block_height,
+    block_width,
+    satellite_solar_angles_fname,
+    out_fname,
+    compression="lzf",
+    solar_source=True,
+):
+    """A private wrapper for dealing with the internal custom workings of the
     NBAR workflow.
     """
-    with h5py.File(dsm_fname, 'r') as dsm_src,\
-        h5py.File(satellite_solar_angles_fname, 'r') as sat_sol:
-
-        dsm_dset = dsm_src['dsm-smoothed']
+    with h5py.File(dsm_fname, "r") as dsm_src, h5py.File(
+        satellite_solar_angles_fname, "r"
+    ) as sat_sol:
+        dsm_dset = dsm_src["dsm-smoothed"]
 
         if solar_source:
-            zenith_name = 'solar-zenith'
-            azimuth_name = 'solar-azimuth'
+            azimuth_name = "solar-azimuth"
         else:
-            zenith_name = 'satellite-view'
-            azimuth_name = 'satellite-azimuth'
+            azimuth_name = "satellite-azimuth"
 
-        view_dset = sat_sol[view_name]
+        sat_sol[view_name]
         azi_dset = sat_sol[azimuth_name]
 
-    fid = calculate_cast_shadow(acquisition, dsm_dset, margins, block_height,
-                                block_width, zenith_dset, azi_dset, out_fname,
-                                compression, solar_source)
+    fid = calculate_cast_shadow(
+        acquisition,
+        dsm_dset,
+        margins,
+        block_height,
+        block_width,
+        zenith_dset,
+        azi_dset,
+        out_fname,
+        compression,
+        solar_source,
+    )
 
     fid.close()
     return
 
 
-def calculate_cast_shadow(acquisition, dsm_dataset, margins, block_height,
-                          block_width, zenith_angle_dataset,
-                          azimuth_angle_dataset, out_fname=None,
-                          compression='lzf', solar_source=True):
-    """
-    This code is an interface to the fortran code
+def calculate_cast_shadow(
+    acquisition,
+    dsm_dataset,
+    margins,
+    block_height,
+    block_width,
+    zenith_angle_dataset,
+    azimuth_angle_dataset,
+    out_fname=None,
+    compression="lzf",
+    solar_source=True,
+):
+    """This code is an interface to the fortran code
     cast_shadow_main.f90 written by Fuqin (and modified to
     work with F2py).
 
@@ -435,37 +459,53 @@ def calculate_cast_shadow(acquisition, dsm_dataset, margins, block_height,
     pixel_buf = ImageMargins(margins)
 
     # Compute the cast shadow mask
-    ierr, mask = cast_shadow_main(dsm, zenith_angle, azimuth_angle, x_res,
-                                  y_res, spheroid, y_origin, x_origin,
-                                  pixel_buf.left, pixel_buf.right,
-                                  pixel_buf.top, pixel_buf.bottom,
-                                  block_height, block_width, is_utm)
+    ierr, mask = cast_shadow_main(
+        dsm,
+        zenith_angle,
+        azimuth_angle,
+        x_res,
+        y_res,
+        spheroid,
+        y_origin,
+        x_origin,
+        pixel_buf.left,
+        pixel_buf.right,
+        pixel_buf.top,
+        pixel_buf.bottom,
+        block_height,
+        block_width,
+        is_utm,
+    )
 
     if ierr:
         raise CastShadowError(ierr)
 
-    source_dir = 'sun' if solar_source else 'satellite'
+    source_dir = "sun" if solar_source else "satellite"
 
     # Initialise the output file
     if out_fname is None:
-        fid = h5py.File('cast-shadow-{}.h5'.format(source_dir), driver='core',
-                        backing_store=False)
+        fid = h5py.File(
+            f"cast-shadow-{source_dir}.h5", driver="core", backing_store=False
+        )
     else:
-        fid = h5py.File(out_fname, 'w')
+        fid = h5py.File(out_fname, "w")
 
-    kwargs = dataset_compression_kwargs(compression=compression,
-                                        chunks=(1, geobox.x_size()))
-    kwargs['dtype'] = 'bool'
+    kwargs = dataset_compression_kwargs(
+        compression=compression, chunks=(1, geobox.x_size())
+    )
+    kwargs["dtype"] = "bool"
 
-    out_dset = fid.create_dataset('cast-shadow-{}'.format(source_dir),
-                                  data=mask, **kwargs)
+    out_dset = fid.create_dataset(f"cast-shadow-{source_dir}", data=mask, **kwargs)
 
     # attach some attributes to the image datasets
-    attrs = {'crs_wkt': geobox.crs.ExportToWkt(),
-             'geotransform': geobox.affine.to_gdal()}
-    desc = ("The cast shadow mask determined using the {} "
-            "as the source direction.").format(source_dir)
-    attrs['Description'] = desc
+    attrs = {
+        "crs_wkt": geobox.crs.ExportToWkt(),
+        "geotransform": geobox.affine.to_gdal(),
+    }
+    desc = (
+        "The cast shadow mask determined using the {} " "as the source direction."
+    ).format(source_dir)
+    attrs["Description"] = desc
     attach_image_attributes(out_dset, attrs)
 
     fid.flush()
