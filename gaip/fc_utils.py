87c5268400de2970f1cf527ed0f964fc88a465a1
Jeremy Hooke <jez@stulk.com>
2017-02-24 17:15:53 +1100

    Default gaip import shouldn't require Fortran mods

--- gaip/fc_utils.py
+++ gaip/fc_utils.py
@@ -5,7 +5,7 @@ import numexpr
 import numpy as np
 
 # from eotools import tiling
-from gaip import GriddedGeoBox, LandsatAcquisition, endmembers, stack_data, unmiximage
+from gaip import GriddedGeoBox, LandsatAcquisition, endmembers, stack_data
 
 # from datacube.api.model import BANDS
 # from datacube.api.model import DatasetType, Satellite
@@ -457,6 +457,8 @@ def unmix(green, red, nir, swir1, swir2, sum_to_one_weight, endmembers_array):
 
     in_null = 0.0001
     out_unmix_null = -10.0
+
+    from gaip import unmiximage
 
     fractions = unmiximage.unmiximage(
         weighted_spectra, endmembers_array, in_null, out_unmix_null
