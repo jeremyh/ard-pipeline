24d8b08699a7b0376d949d1e3b14e236e856d18c
Josh Sixsmith <joshua.sixsmith@ga.gov.au>
2017-03-06 13:49:01 +1100

    ImportError now uses the print function.

--- gaip/__init__.py
+++ gaip/__init__.py
@@ -1,50 +1,38 @@
-
-from hdf5 import *
-from data import *
-from metadata import *
-from ancillary import *
 from acquisition import *
-from mtl import *
-from geobox import GriddedGeoBox
-from tle import *
+from ancillary import *
 from brdf import *
 from calculate_lon_lat_arrays import *
-from land_sea_masking import *
-from land_sea import get_land_sea_mask
-from modtran_profiles import *
-from modtran import *
-from margins import *
-from constants import PQAConstants
-from pqa_result import PQAResult
-from saturation_masking import set_saturation_bits
-from contiguity_masking import set_contiguity_bit
-from fc_utils import *
-from endmembers import *
-from version import get_version
-from thermal_conversion import get_landsat_temperature
-from acca_cloud_masking import calc_acca_cloud_mask
-from acca_cloud_masking import majority_filter
-from fmask_cloud_masking_wrapper import fmask_cloud_mask
-from cloud_shadow_masking import cloud_shadow
+from data import *
 from dsm import *
+from endmembers import *
+from fc_utils import *
+from hdf5 import *
+from land_sea_masking import *
+from margins import *
+from metadata import *
+from modtran import *
+from modtran_profiles import *
+from mtl import *
+from tle import *
 
 try:
-    from _satellite_model import set_satmod # F2Py
-    from _track_time_info import set_times # F2Py
-    from _sat_sol_angles import angle # F2Py
-    from _cast_shadow_mask import cast_shadow_main # F2Py
-    from _exiting_angle import exiting_angle # F2Py
-    from _incident_angle import incident_angle # F2Py
-    from _slope_aspect import slope_aspect # F2Py
-    from _surface_reflectance import reflectance # F2Py
-    from _interpolation import bilinear #F2Py
+    from _cast_shadow_mask import cast_shadow_main  # F2Py
+    from _exiting_angle import exiting_angle  # F2Py
+    from _incident_angle import incident_angle  # F2Py
+    from _interpolation import bilinear  # F2Py
+    from _sat_sol_angles import angle  # F2Py
+    from _satellite_model import set_satmod  # F2Py
+    from _slope_aspect import slope_aspect  # F2Py
+    from _surface_reflectance import reflectance  # F2Py
+    from _track_time_info import set_times  # F2Py
     from calculate_angles import *
-    from calculate_shadow_masks import *
-    from calculate_reflectance import calculate_reflectance
     from calculate_incident_exiting_angles import *
+    from calculate_reflectance import calculate_reflectance
+    from calculate_shadow_masks import *
     from calculate_slope_aspect import *
 except ImportError:
-    msg = ('Run Makefile to build the Fortran modules.\n'
-           'Some functionality in library is disabled')
-    import sys
-    print sys.stderr, msg
+    msg = (
+        "FORTRAN modules have not been built.\n"
+        "Some functionality in library is disabled"
+    )
+    print(msg)
