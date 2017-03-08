f27450a54d34b164085093b679e7f463c1303202
Josh Sixsmith <joshua.sixsmith@ga.gov.au>
2017-03-08 17:53:34 +1100

    Function name changes.

--- gaip/tests/test_random_locations.py
+++ gaip/tests/test_random_locations.py
@@ -9,14 +9,14 @@ class TestRandomPixelLocations(unittest.TestCase):
     def test_non_2D(self):
         """Test that specifying a non 2D tuple raises a TypeEror."""
         dims = (3, 100, 100)
-        self.assertRaises(TypeError, ut.randomPixelLocations, dims)
+        self.assertRaises(TypeError, ut.random_pixel_locations, dims)
 
-    def test_nPixels(self):
+    def test_n_pixels(self):
         """Test that the correct number of random pixels are returned.
         The default return value is 100 pixels.
         """
         dims = (100, 100)
-        idx = ut.randomPixelLocations(dims)
+        idx = ut.random_pixel_locations(dims)
         n = idx[0].shape[0]
         assert n == 100
 
