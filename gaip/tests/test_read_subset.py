c8b3c10646c60b2e8fd6d3b1be71fffc13da8732
Josh Sixsmith <josh.sixsmith@gmail.com>
2017-03-11 00:04:11 +1100

    Keyword fix.

--- gaip/tests/test_read_subset.py
+++ gaip/tests/test_read_subset.py
@@ -32,7 +32,7 @@ class TestReadSubset(unittest.TestCase):
         LR = geobox.convert_coordinates((9, 10))
         LL = geobox.convert_coordinates((-9, 10))
 
-        kwds = {"fname": fname, "ul xy": UL, "ur xy": UR, "lr xy": LR, "ll xy": LL}
+        kwds = {"fname": fname, "ul_xy": UL, "ur_xy": UR, "lr_xy": LR, "ll_xy": LL}
 
         self.assertRaises(IndexError, read_subset, **kwds)
 
