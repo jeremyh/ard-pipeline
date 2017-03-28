5646dd412d1b8ac62b50ebc750a4482ffb0272dd
Josh Sixsmith <joshua.sixsmith@ga.gov.au>
2017-03-28 11:55:05 +1100

    Updated doc string.

--- gaip/calculate_angles.py
+++ gaip/calculate_angles.py
@@ -241,7 +241,9 @@ def create_boxline_coordinator(
                                     ('start_index', 'int64'),
                                     ('end_index', 'int64')])
         * coordinator_dtype = np.dtype([('row_index', 'int64'),
-                                        ('col_index', 'int64')])
+                                        ('col_index', 'int64'),
+                                        ('latitude', 'float64'),
+                                        ('longitude', 'float64')])
     """
     rows, cols = view_angle_dataset.shape
 
