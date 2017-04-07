9d7a382e7af70cb8843b6acf308193b23eb88cf8
Josh Sixsmith <josh.sixsmith@gmail.com>
2017-04-07 21:14:51 +1000

    Added docstrings.

--- gaip/constants.py
+++ gaip/constants.py
@@ -41,6 +41,9 @@ class Model(Enum):
 
     @property
     def factors(self):
+        """Returns the factor names used for interpolation for a given
+        Model.<option>.
+        """
         fmap = {
             Model.standard: ALL_FACTORS,
             Model.nbar: ALL_FACTORS[0:8],
@@ -50,6 +53,9 @@ class Model(Enum):
 
     @property
     def albedos(self):
+        """Returns the albedo names used for specific Atmospheric
+        evaluations for a given Model.<option>.
+        """
         amap = {
             Model.standard: ALL_ALBEDOS,
             Model.nbar: ALL_ALBEDOS[0:-1],
@@ -68,7 +74,7 @@ class BandType(Enum):
     Quality = 4
 
 
-class DatasetNames(Enum):
+class DatasetName(Enum):
     """Defines the dataset names or format descriptors, that are used
     for creating and accessing throughout the code base.
     """
