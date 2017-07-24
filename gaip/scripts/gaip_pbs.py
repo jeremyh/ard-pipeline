d34a7f2c4222d1a6212e743e67d32fd42fd09e23
Josh Sixsmith <josh.sixsmith@gmail.com>
2017-07-24 22:14:43 +1000

    Added jobfs resources request.

--- gaip/scripts/gaip_pbs.py
+++ gaip/scripts/gaip_pbs.py
@@ -14,7 +14,7 @@ from gaip.tiling import scatter
 PBS_TEMPLATE = """#!/bin/bash
 #PBS -P {project}
 #PBS -q {queue}
-#PBS -l walltime={hours}:00:00,mem={memory}GB,ncpus={ncpus}
+#PBS -l walltime={hours}:00:00,mem={memory}GB,ncpus={ncpus},jobfs=50GB
 #PBS -l wd
 #PBS -me
 #PBS -M {email}
@@ -29,7 +29,7 @@ luigi --module gaip.{workflow} ARD --model {model} --level1-list {scene_list} --
 DSH_TEMPLATE = """#!/bin/bash
 #PBS -P {project}
 #PBS -q {queue}
-#PBS -l walltime={hours}:00:00,mem={memory}GB,ncpus={ncpus}
+#PBS -l walltime={hours}:00:00,mem={memory}GB,ncpus={ncpus},jobfs=50GB,other=pernodejobfs
 #PBS -l wd
 #PBS -me
 #PBS -M {email}
