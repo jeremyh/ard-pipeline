2a85055fed9e1108c660c1fd462bf67c1ee7c637
Josh Sixsmith <josh.sixsmith@gmail.com>
2017-09-11 10:45:27 +1000

    Updated the ancillary retrieval for the case of ancillaries that have been aggregated.

--- gaip/modtran.py
+++ gaip/modtran.py
@@ -120,12 +120,17 @@ def format_tp5(
     longitude = lon_lat_group[DatasetName.lon.value]
     latitude = lon_lat_group[DatasetName.lat.value]
 
+    # retrieve the averaged ancillary if available
+    anc_grp = ancillary_group.get(GroupName.ancillary_avg_group.value)
+    if anc_grp is None:
+        anc_grp = ancillary_group
+
     # ancillary data
     coordinator = ancillary_group[DatasetName.coordinator.value]
-    aerosol = ancillary_group[DatasetName.aerosol.value][()]
-    water_vapour = ancillary_group[DatasetName.water_vapour.value][()]
-    ozone = ancillary_group[DatasetName.ozone.value][()]
-    elevation = ancillary_group[DatasetName.elevation.value][()]
+    aerosol = anc_grp[DatasetName.aerosol.value][()]
+    water_vapour = anc_grp[DatasetName.water_vapour.value][()]
+    ozone = anc_grp[DatasetName.ozone.value][()]
+    elevation = anc_grp[DatasetName.elevation.value][()]
 
     npoints = coordinator.shape[0]
     view = np.zeros(npoints, dtype="float32")
