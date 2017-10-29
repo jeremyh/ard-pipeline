b7f7591b169de6831451148702781e0a8bfb394a
Josh Sixsmith <josh.sixsmith@gmail.com>
2017-10-29 20:44:56 +1100

    Updated docstring to account for the removal of the dsm output.

--- gaip/dsm.py
+++ gaip/dsm.py
@@ -74,9 +74,8 @@ def get_dsm(acquisition, national_dsm, margins, out_group=None, compression="lzf
         as an in-memory hdf5 file, i.e. the `core` driver. Otherwise,
         a writeable HDF5 `Group` object.
 
-        The dataset names will be as follows:
+        The dataset name will be as follows:
 
-        * DatasetName.dsm
         * DatasetName.dsm_smoothed
 
     :param compression:
