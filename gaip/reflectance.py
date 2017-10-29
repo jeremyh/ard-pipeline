2c94017903d79e149099d981ecf9597a1743c249
Josh Sixsmith <josh.sixsmith@gmail.com>
2017-10-30 10:13:34 +1100

    Added an acq.close() statement for general close and cleanup on the acquisition.

--- gaip/reflectance.py
+++ gaip/reflectance.py
@@ -382,6 +382,9 @@ def calculate_reflectance(
         nbar_dset[idx] = ref_brdf
         nbart_dset[idx] = ref_terrain
 
+    # close any still opened files, arrays etc associated with the acquisition
+    acq.close()
+
     if out_group is None:
         return fid
 
