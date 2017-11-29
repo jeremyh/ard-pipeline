8ea340e167c9f095f5067882c9602fab3ff69ef5
Alex Vincent <alexander.vincent@ga.gov.au>
2017-11-30 09:32:18 +1100

    fix for hardcoded path in s2 directory acquisition

--- gaip/acquisition/__init__.py
+++ gaip/acquisition/__init__.py
@@ -277,9 +277,7 @@ def acquisitions_s2_directory(pathname):
 
     acquisition_data = {}
     for fn, terms in search_paths.items():
-        xml_root = None
-        with open("/home/ubuntu/granule/" + fn) as fd:
-            xml_root = ElementTree.XML(fd.read())
+        xml_root = ElementTree.parse(pathname + "/" + fn)
         for term in terms:
             acquisition_data[term["key"]] = term["parse"](
                 xml_root.findall(term["search_path"])
