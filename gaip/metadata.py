6797debab6e044c500d499e01cbc41f01a6ec24c
Josh Sixsmith <josh.sixsmith@gmail.com>
2017-12-06 11:34:17 +1100

    Convert UUID to str for yaml output.

--- gaip/metadata.py
+++ gaip/metadata.py
@@ -253,7 +253,7 @@ def create_ard_yaml(acquisitions, ancillary_group, out_group, sbt=False):
     system_info = {
         "uname": " ".join(os.uname()),
         "hostname": socket.getfqdn(),
-        "runtime_id": uuid.uuid1(),
+        "runtime_id": str(uuid.uuid1()),
         "time_processed": dtime.utcnow().isoformat(),
     }
 
@@ -293,7 +293,7 @@ def create_pq_yaml(acquisition, ancillary, tests_run, out_group):
     system_info = {
         "uname": " ".join(os.uname()),
         "hostname": socket.getfqdn(),
-        "runtime_id": uuid.uuid1(),
+        "runtime_id": str(uuid.uuid1()),
         "time_processed": dtime.utcnow().isoformat(),
     }
 
