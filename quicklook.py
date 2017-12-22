e58153ef4ef4973fc04696dc449ace8b6818264b
Josh Sixsmith <josh.sixsmith@gmail.com>
2017-12-22 13:31:53 +1100

    Remove the duplication of the metadata dict, instead just overwrite the image bands section with the s3 paths so as to keep the uuids the same between documents.

--- quicklook.py
+++ quicklook.py
@@ -1,5 +1,5 @@
 """
-Execution method for quicklook creatiob
+Execution method for quicklook creation
 
 example usage:
     quicklook.py red.tif green.tif blue.tif --output quicklook.tif --scale 0 3500 0 255
@@ -60,7 +60,7 @@ def make_quicklook(datasets, src_min, src_max, null, output):
 )
 def main(output, datasets):
     """
-    For input colour bands generate full resolution quickook
+    For input colour bands generate full resolution quicklook
     output in 8/24 bit colour and write to the destination path specified by 'output'
     """
     logging.basicConfig(
