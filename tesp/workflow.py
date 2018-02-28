7bf7a794b571a745630d142fc550348abe10b270
Josh Sixsmith <josh.sixsmith@gmail.com>
2018-02-28 21:22:30 +1100

    Fixed import of fmask. Updated L1GTCP tag renaming to use the re pattern defined in the package module.

--- tesp/workflow.py
+++ tesp/workflow.py
@@ -3,6 +3,7 @@
 """A temporary workflow for processing S2 data into an ARD package."""
 
 import logging
+import re
 import shutil
 import traceback
 from os.path import basename, dirname
@@ -10,6 +11,7 @@ from os.path import join as pjoin
 from pathlib import Path
 
 import luigi
+from eugl.fmask import fmask
 from luigi.contrib.s3 import S3Client, S3FlagTarget
 from luigi.local_target import LocalFileSystem
 from luigi.util import inherits
@@ -21,8 +23,7 @@ from structlog.processors import JSONRenderer
 from wagl.acquisition import acquisitions
 from wagl.singlefile_workflow import DataStandardisation
 
-from tesp.fmask_cophub import fmask
-from tesp.package import package
+from tesp.package import ARD, PATTERN2, package
 
 ERROR_LOGGER = wrap_logger(
     logging.getLogger("ard-error"), processors=[JSONRenderer(indent=1, sort_keys=True)]
@@ -132,8 +133,8 @@ class Package(luigi.Task):
         return tasks
 
     def output(self):
-        granule = self.granule if self.granule else ""
-        out_fname = pjoin(self.pkgdir, granule.replace("L1C", "ARD"), "CHECKSUM.sha1")
+        granule = re.sub(PATTERN2, ARD, self.granule)
+        out_fname = pjoin(self.pkgdir, granule, "CHECKSUM.sha1")
 
         return luigi.LocalTarget(out_fname)
 
