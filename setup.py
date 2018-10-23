c368afc9c0d24f17b9cedaf8c3287d4adb138528
Alex Vincent <2034948+ASVincent@users.noreply.github.com>
2018-10-23 13:34:12 +1100

    Update for travis sdist build (#21)

--- setup.py
+++ setup.py
@@ -5,8 +5,13 @@ from setuptools import find_packages, setup
 
 setup(
     name="eugl",
-    version=versioneer.get_version(),
     description="Modules that deal with sensor and data quality characterisation.",
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    url="https://github.com/OpenDataCubePipelines/eugl",
+    author="The wagl authors",
+    author_email="earth.observation@ga.gov.au",
+    maintainer="wagl developers",
     packages=find_packages(),
     install_requires=[
         "eodatasets",
@@ -16,10 +21,13 @@ setup(
         "rasterio",
         "rios",
         "python-fmask",
+        "wagl",
     ],
     package_data={"eugl.gqa": ["data/*.csv"]},
     dependency_links=[
         "hg+https://bitbucket.org/chchrsc/rios/get/rios-1.4.5.zip#egg=rios-1.4.5",
-        "hg+https://bitbucket.org/chchrsc/python-fmask/get/python-fmask-0.4.5.zip#egg=python-fmask-0.4.5",
+        "hg+https://bitbucket.org/chchrsc/python-fmask/get/python-fmask-0.4.5.zip#egg=python-fmask-0.4.5"
+        "git+https://github.com/GeoscienceAustralia/wagl@master#egg=wagl",
+        "git+https://github.com/GeoscienceAustralia/eo-datasets.git@develop#egg=eodatasets",
     ],
 )
