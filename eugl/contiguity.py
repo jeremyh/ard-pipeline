a17e8abb88158aec36dc698575fab39d26f99035
passangd <41933342+passangd@users.noreply.github.com>
2019-01-23 13:14:24 +1100

    Update contiguity.py
    addition of sensor platform in main function of contiguity.py

--- eugl/contiguity.py
+++ eugl/contiguity.py
@@ -1,16 +1,17 @@
-"""Execution method for contiguous observations within band stack.
+"""
+Execution method for contiguous observations within band stack
 
 example usage:
     contiguity.py <allbands.vrt>
     --output /tmp/
 """
-import logging
+from __future__ import absolute_import
 import os
+import logging
 from pathlib import Path
-
-import click
-import numpy as np
 import rasterio
+import numpy as np
+import click
 from wagl.data import write_img
 from wagl.geobox import GriddedGeoBox
 
@@ -18,13 +19,14 @@ os.environ["CPL_ZIP_ENCODING"] = "UTF-8"
 
 
 def contiguity(fname, output, platform):
-    """Write a contiguity mask file based on the intersection of valid data pixels across all
-    bands from the input file and output to the specified directory.
+    """
+    Write a contiguity mask file based on the intersection of valid data pixels across all
+    bands from the input file and output to the specified directory
     """
     with rasterio.open(fname) as ds:
         geobox = GriddedGeoBox.from_dataset(ds)
         yblock, xblock = ds.block_shapes[0]
-        ones = np.ones((ds.height, ds.width), dtype="uint8")
+        ones = np.ones((ds.height, ds.width), dtype='uint8')
         for band in ds.indexes:
             ones &= ds.read(band) > 0
 
@@ -39,54 +41,40 @@ def contiguity(fname, output, platform):
     if platform == "LANDSAT":
         blockxsize = 512
         blockysize = 512
-        config_options = {"GDAL_TIFF_OVR_BLOCKSIZE": blockxsize}
+        config_options = {'GDAL_TIFF_OVR_BLOCKSIZE': blockxsize}
     else:
         blockysize = yblock
         blockxsize = xblock
         config_options = None
 
-    options = {
-        "compress": "deflate",
-        "zlevel": 4,
-        "blockxsize": blockxsize,
-        "blockysize": blockysize,
-    }
+    options = {'compress': 'deflate',
+               'zlevel': 4,
+               'blockxsize': blockxsize,
+               'blockysize': blockysize}
 
-    write_img(
-        ones,
-        output,
-        cogtif=True,
-        levels=[2, 4, 8, 16, 32],
-        geobox=geobox,
-        options=options,
-        config_options=config_options,
-    )
+    write_img(ones, output, cogtif=True, levels=[2, 4, 8, 16, 32],
+              geobox=geobox, options=options, config_options=config_options)
 
     return ones
 
 
 @click.command(help=__doc__)
-@click.option(
-    "--output",
-    help="Write contiguity datasets into this directory",
-    type=click.Path(exists=False, writable=True, dir_okay=True),
-)
-@click.argument(
-    "datasets", type=click.Path(exists=True, readable=True, writable=False), nargs=-1
-)
-def main(output, datasets):
-    """For input 'vrt' generate Contiguity
-    outputs and write to the destination path specified by 'output'.
+@click.option('--output', help="Write contiguity datasets into this directory",
+              type=click.Path(exists=False, writable=True, dir_okay=True))
+@click.argument('datasets',
+                type=click.Path(exists=True, readable=True, writable=False),
+                nargs=-1)
+@click.option('--platform', help=" Sensor platform where dataset is source from.', default=None)
+def main(output, datasets, platform):
     """
-    logging.basicConfig(
-        format="%(asctime)s %(levelname)s %(message)s", level=logging.INFO
-    )
+    For input 'vrt' generate Contiguity
+    outputs and write to the destination path specified by 'output'
+    """
+    logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s', level=logging.INFO)
     for dataset in datasets:
         path = dataset
         stem = Path(path).stem
         out = os.path.join(output, stem)
         contiguity_img = out + ".CONTIGUITY.TIF"
         logging.info("Create contiguity image %s", contiguity_img)
-
-        # TODO needs platform to be passed too
-        contiguity(path, contiguity_img)
+        contiguity(path, contiguity_img, plaform)
