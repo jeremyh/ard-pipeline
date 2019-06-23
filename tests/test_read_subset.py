36e7ff3b68c7f74af785eed94095488b11dce2cf
Josh Sixsmith <josh.sixsmith@gmail.com>
2019-06-24 09:04:39 +1000

    Updated read_subset tests.

--- tests/test_read_subset.py
+++ tests/test_read_subset.py
@@ -1,36 +1,32 @@
 #!/usr/bin/env python
 
-from __future__ import absolute_import
 import os
 import shutil
 import tempfile
 import unittest
 
-import numpy
 import h5py
+import numpy as np
 
-from wagl.data import read_subset
-from wagl.data import write_img
 from wagl import unittesting_tools as ut
+from wagl.data import read_subset, write_img
 
 
 class TestReadSubset(unittest.TestCase):
-
     img, geobox = ut.create_test_image((1200, 1500))
     img[:] = 1
 
-    fid = h5py.File('test-subset.h5', backing_store=False, driver='core')
-    ds = fid.create_dataset('data', data=img)
-    ds.attrs['geotransform'] = geobox.transform.to_gdal()
-    ds.attrs['crs_wkt'] = geobox.crs.ExportToWkt()
-    ds.attrs['fillvalue'] = 0
+    fid = h5py.File("test-subset.h5", backing_store=False, driver="core")
+    ds = fid.create_dataset("data", data=img)
+    ds.attrs["geotransform"] = geobox.transform.to_gdal()
+    ds.attrs["crs_wkt"] = geobox.crs.ExportToWkt()
+    ds.attrs["fillvalue"] = 0
 
     subs_shape = (200, 300)
 
     @unittest.skip("Refactor DSM subsetting logic; TODO update test")
     def testWestBounds(self):
-        """
-        Test that a co-ordinate west of the image domain returns an
+        """Test that a co-ordinate west of the image domain returns an
         index error.
         The subset attempts to read a 20 by 20 block with half contained
         within the image bounds and half contained outside the image
@@ -40,7 +36,7 @@ class TestReadSubset(unittest.TestCase):
 
         # Temporarily write the image to disk
         temp_dir = tempfile.mkdtemp()
-        fname = os.path.join(temp_dir, 'testWestBounds')
+        fname = os.path.join(temp_dir, "testWestBounds")
         write_img(img, fname, geobox=geobox)
 
         # Create box to read 10 pixels left of the image bounds
@@ -49,25 +45,19 @@ class TestReadSubset(unittest.TestCase):
         LR = geobox.convert_coordinates((9, 10))
         LL = geobox.convert_coordinates((-9, 10))
 
-        kwds = {'fname': fname,
-                 'ul_xy': UL,
-                 'ur_xy': UR,
-                 'lr_xy': LR,
-                 'll_xy': LL}
+        kwds = {"fname": fname, "ul_xy": UL, "ur_xy": UR, "lr_xy": LR, "ll_xy": LL}
 
         self.assertRaises(IndexError, read_subset, **kwds)
 
         # Cleanup
         shutil.rmtree(temp_dir)
 
-
     @unittest.skip("Refactor DSM subsetting logic; TODO update test")
     def testEastBounds(self):
-        """
-        Test that a co-ordinate east of the image domain returns an
+        """Test that a co-ordinate east of the image domain returns an
         index error.
         The subset attempts to read a 20 by 20 block with half contained
-        within the image bounds and half contained outside the image
+        within the image bounds and half contained outside the image.
         """
         img, geobox = ut.create_test_image()
 
@@ -75,40 +65,34 @@ class TestReadSubset(unittest.TestCase):
 
         # Temporarily write the image to disk
         temp_dir = tempfile.mkdtemp()
-        fname = os.path.join(temp_dir, 'testEastBounds')
+        fname = os.path.join(temp_dir, "testEastBounds")
         write_img(img, fname, geobox=geobox)
 
         # Create box to read 10 pixels right of the image bounds
-        UL = geobox.convert_coordinates((cols-9, 0))
-        UR = geobox.convert_coordinates((cols+10, 0))
-        LR = geobox.convert_coordinates((cols+10, 10))
-        LL = geobox.convert_coordinates((cols-9, 10))
+        UL = geobox.convert_coordinates((cols - 9, 0))
+        UR = geobox.convert_coordinates((cols + 10, 0))
+        LR = geobox.convert_coordinates((cols + 10, 10))
+        LL = geobox.convert_coordinates((cols - 9, 10))
 
-        kwds = {'fname': fname,
-                 'ul_xy': UL,
-                 'ur_xy': UR,
-                 'lr_xy': LR,
-                 'll_xy': LL}
+        kwds = {"fname": fname, "ul_xy": UL, "ur_xy": UR, "lr_xy": LR, "ll_xy": LL}
 
         self.assertRaises(IndexError, read_subset, **kwds)
 
         # Cleanup
         shutil.rmtree(temp_dir)
 
-
     @unittest.skip("Refactor DSM subsetting logic; TODO update test")
     def testNorthBounds(self):
-        """
-        Test that a co-ordinate north of the image domain returns an
+        """Test that a co-ordinate north of the image domain returns an
         index error.
         The subset attempts to read a 20 by 20 block with half contained
-        within the image bounds and half contained outside the image
+        within the image bounds and half contained outside the image.
         """
         img, geobox = ut.create_test_image()
 
         # Temporarily write the image to disk
         temp_dir = tempfile.mkdtemp()
-        fname = os.path.join(temp_dir, 'testNorthBounds')
+        fname = os.path.join(temp_dir, "testNorthBounds")
         write_img(img, fname, geobox=geobox)
 
         # Create box to read 10 pixels above the image bounds
@@ -117,25 +101,19 @@ class TestReadSubset(unittest.TestCase):
         LR = geobox.convert_coordinates((10, 10))
         LL = geobox.convert_coordinates((0, 10))
 
-        kwds = {'fname': fname,
-                 'ul_xy': UL,
-                 'ur_xy': UR,
-                 'lr_xy': LR,
-                 'll_xy': LL}
+        kwds = {"fname": fname, "ul_xy": UL, "ur_xy": UR, "lr_xy": LR, "ll_xy": LL}
 
         self.assertRaises(IndexError, read_subset, **kwds)
 
         # Cleanup
         shutil.rmtree(temp_dir)
 
-
     @unittest.skip("Refactor DSM subsetting logic; TODO update test")
     def testSouthBounds(self):
-        """
-        Test that a co-ordinate south of the image domain returns an
+        """Test that a co-ordinate south of the image domain returns an
         index error.
         The subset attempts to read a 20 by 20 block with half contained
-        within the image bounds and half contained outside the image
+        within the image bounds and half contained outside the image.
         """
         img, geobox = ut.create_test_image()
 
@@ -143,31 +121,25 @@ class TestReadSubset(unittest.TestCase):
 
         # Temporarily write the image to disk
         temp_dir = tempfile.mkdtemp()
-        fname = os.path.join(temp_dir, 'testSouthBounds')
+        fname = os.path.join(temp_dir, "testSouthBounds")
         write_img(img, fname, geobox=geobox)
 
         # Create box to read 10 pixels below the image bounds
-        UL = geobox.convert_coordinates((0, rows-9))
-        UR = geobox.convert_coordinates((10, rows-9))
-        LR = geobox.convert_coordinates((10, rows+10))
-        LL = geobox.convert_coordinates((0, rows+10))
+        UL = geobox.convert_coordinates((0, rows - 9))
+        UR = geobox.convert_coordinates((10, rows - 9))
+        LR = geobox.convert_coordinates((10, rows + 10))
+        LL = geobox.convert_coordinates((0, rows + 10))
 
-        kwds = {'fname': fname,
-                 'ul_xy': UL,
-                 'ur_xy': UR,
-                 'lr_xy': LR,
-                 'll_xy': LL}
+        kwds = {"fname": fname, "ul_xy": UL, "ur_xy": UR, "lr_xy": LR, "ll_xy": LL}
 
         self.assertRaises(IndexError, read_subset, **kwds)
 
         # Cleanup
         shutil.rmtree(temp_dir)
 
-
-    @unittest.skip('Requires refactoring')
+    @unittest.skip("Requires refactoring")
     def test_correct_subset(self):
-        """
-        Test that the subset is what we expect.
+        """Test that the subset is what we expect.
         Read a 10 by 10 starting at the UL corner.
         """
         img, geobox = ut.create_test_image()
@@ -176,7 +148,7 @@ class TestReadSubset(unittest.TestCase):
 
         # Temporarily write the image to disk
         temp_dir = tempfile.mkdtemp()
-        fname = os.path.join(temp_dir, 'test_image')
+        fname = os.path.join(temp_dir, "test_image")
         write_img(img, fname, geobox=geobox)
 
         # Create box to read 10 pixels below the image bounds
@@ -185,37 +157,32 @@ class TestReadSubset(unittest.TestCase):
         LR = geobox.convert_coordinates((9, 9))
         LL = geobox.convert_coordinates((0, 9))
 
-        kwds = {'fname': fname,
-                 'ul_xy': UL,
-                 'ur_xy': UR,
-                 'lr_xy': LR,
-                 'll_xy': LL}
+        kwds = {"fname": fname, "ul_xy": UL, "ur_xy": UR, "lr_xy": LR, "ll_xy": LL}
 
         subs, geobox = read_subset(**kwds)
 
-        base = img[0:10,0:10]
+        base = img[0:10, 0:10]
 
-        result = numpy.sum(base - subs)
+        result = np.sum(base - subs)
 
-        self.assertTrue(result == 0)
+        assert result == 0
 
         # Cleanup
         shutil.rmtree(temp_dir)
 
     def test_case_a(self):
-        """
-        Origin = (-150, -50)
+        """Origin = (-150, -50); `O`.
 
-            +----+
-            -    -
-            -  +-----------+
-            -  - -         -
-            +----+         -
-               -           -
-               -           -
-               -           -
-               -           -
-               +-----------+
+        O+----+
+        -    -
+        -  +-----------+
+        -  - -         -
+        +----+         -
+        -           -
+        -           -
+        -           -
+        -           -
+        +-----------+
         """
         # indices based on full array
         ul = (-150, -50)
@@ -232,14 +199,13 @@ class TestReadSubset(unittest.TestCase):
         # read subset
         data, gb = read_subset(self.ds, ul_xy_map, ur_xy_map, lr_xy_map, ll_xy_map)
 
-        count =
-        self.assertTrue(data.sum() == count)
+        count = 50 * 250
+        assert data.sum() == count
 
     def test_case_b(self):
-        """
-        Origin = (-150, 600)
+        """Origin = (-150, 600); `O`.
 
-            +---+
+           O+---+
             -   -
         +----   ----+
         -   -   -   -
@@ -265,14 +231,13 @@ class TestReadSubset(unittest.TestCase):
         # read subset
         data, gb = read_subset(self.ds, ul_xy_map, ur_xy_map, lr_xy_map, ll_xy_map)
 
-        count =
-        self.assertTrue(data.sum() == count)
+        count = 50 * 300
+        assert data.sum() == count
 
     def test_case_c(self):
-        """
-        Origin = (-150, 1400)
+        """Origin = (-150, 1400); `O`.
 
-                   +---+
+                  O+---+
                    -   -
         +------------+ -
         -          - - -
@@ -298,21 +263,20 @@ class TestReadSubset(unittest.TestCase):
         # read subset
         data, gb = read_subset(self.ds, ul_xy_map, ur_xy_map, lr_xy_map, ll_xy_map)
 
-        count =
-        self.assertTrue(data.sum() == count)
+        count = 50 * 100
+        assert data.sum() == count
 
     def test_case_d(self):
-        """
-        Origin = (600, -50)
+        """Origin = (600, -50); `O`.
 
-              +-----------+
-              -           -
-           +-----+        -
-           -  -  -        -
-           -  -  -        -
-           +-----+        -
-              -           -
-              +-----------+
+        +-----------+
+        -           -
+        O+-----+        -
+        -  -  -        -
+        -  -  -        -
+        +-----+        -
+        -           -
+        +-----------+
         """
         # indices based on full array
         ul = (600, -50)
@@ -329,16 +293,15 @@ class TestReadSubset(unittest.TestCase):
         # read subset
         data, gb = read_subset(self.ds, ul_xy_map, ur_xy_map, lr_xy_map, ll_xy_map)
 
-        count =
-        self.assertTrue(data.sum() == count)
+        count = 200 * 250
+        assert data.sum() == count
 
     def test_case_e(self):
-        """
-        Origin = (600, 600)
+        """Origin = (600, 600); `O`.
 
         +-----------+
         -           -
-        -   +---+   -
+        -  O+---+   -
         -   -   -   -
         -   -   -   -
         -   +---+   -
@@ -360,16 +323,15 @@ class TestReadSubset(unittest.TestCase):
         # read subset
         data, gb = read_subset(self.ds, ul_xy_map, ur_xy_map, lr_xy_map, ll_xy_map)
 
-        count = self.subs_shape[0] * self.subs_shape[1]
-        self.assertTrue(data.sum() == count)
+        count = 200 * 300
+        assert data.sum() == count
 
     def test_case_f(self):
-        """
-        Origin = (600, 1400)
+        """Origin = (600, 1400); `O`.
 
         +-----------+
         -           -
-        -        +-----+
+        -       O+-----+
         -        -  -  -
         -        -  -  -
         -        +-----+
@@ -391,24 +353,23 @@ class TestReadSubset(unittest.TestCase):
         # read subset
         data, gb = read_subset(self.ds, ul_xy_map, ur_xy_map, lr_xy_map, ll_xy_map)
 
-        count =
-        self.assertTrue(data.sum() == count)
+        count = 200 * 100
+        assert data.sum() == count
 
     def test_case_g(self):
-        """
-        Origin = (1100, -50)
+        """Origin = (1100, -50); `O`.
 
-              +-----------+
-              -           -
-              -           -
-              -           -
-              -           -
-           +-----+        -
-           -  -  -        -
-           -  +-----------+
-           -     -
-           -     -
-           +-----+
+        +-----------+
+        -           -
+        -           -
+        -           -
+        -           -
+        O+-----+        -
+        -  -  -        -
+        -  +-----------+
+        -     -
+        -     -
+        +-----+
         """
         # indices based on full array
         ul = (1100, -50)
@@ -425,19 +386,18 @@ class TestReadSubset(unittest.TestCase):
         # read subset
         data, gb = read_subset(self.ds, ul_xy_map, ur_xy_map, lr_xy_map, ll_xy_map)
 
-        count =
-        self.assertTrue(data.sum() == count)
+        count = 100 * 250
+        assert data.sum() == count
 
     def test_case_h(self):
-        """
-        Origin = (1100, 600)
+        """Origin = (1100, 600); `O`.
 
         +-----------+
         -           -
         -           -
         -           -
         -           -
-        -   +----+  -
+        -  O+----+  -
         -   -    -  -
         +-----------+
             -    -
@@ -459,19 +419,18 @@ class TestReadSubset(unittest.TestCase):
         # read subset
         data, gb = read_subset(self.ds, ul_xy_map, ur_xy_map, lr_xy_map, ll_xy_map)
 
-        count =
-        self.assertTrue(data.sum() == count)
+        count = 100 * 300
+        assert data.sum() == count
 
     def test_case_i(self):
-        """
-        Origin = (1100, 1400)
+        """Origin = (1100, 1400); `O`.
 
         +-----------+
         -           -
         -           -
         -           -
         -           -
-        -        +-----+
+        -       O+-----+
         -        -     -
         +-----------+---
                  -     -
@@ -493,8 +452,9 @@ class TestReadSubset(unittest.TestCase):
         # read subset
         data, gb = read_subset(self.ds, ul_xy_map, ur_xy_map, lr_xy_map, ll_xy_map)
 
-        count =
-        self.assertTrue(data.sum() == count)
+        count = 100 * 100
+        assert data.sum() == count
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
