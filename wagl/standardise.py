2e9514c53e31e63a5e36d1c67be1d97f3b6b8559
Josh Sixsmith <josh.sixsmith@gmail.com>
2019-06-27 09:28:47 +1000

    Added a resolution attribute to the resolution Group.  A suggestion by @jeremyh.  The resolution Group isn't explicit about spatial extent, for example the DSM that gets retrieved is buffered by several kilometres, but is the same resolution as the acquisition used for retrieving the DSM.

--- wagl/standardise.py
+++ wagl/standardise.py
@@ -217,6 +217,9 @@ def card4l(
             root = fid.create_group(ppjoin(granule, grp_name))
             acqs = container.get_acquisitions(granule=granule, group=grp_name)
 
+            # include the resolution as a group attribute
+            root.attrs["resolution"] = acqs[0].resolution
+
             # longitude and latitude
             log.info("Latitude-Longitude")
             create_lon_lat_grids(acqs[0], root, compression, filter_opts)
