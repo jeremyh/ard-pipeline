5e1db632ec461a85dbe2b48bae806441341a04bd
Imam Alam <imam.alam@gmail.com>
2021-02-18 00:51:48 +0000

    simplify runner

--- modtran/modtran_runner/modtran_runner.sh
+++ modtran/modtran_runner/modtran_runner.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-echo "This is the modtran runner starting!"
-echo $MODTRAN_DATA
-echo $MODTRAN_PRODUCT_KEY
+export MOD6=/modtran6/bin/linux/mod6c_cons
+$MOD6 -version
+$MOD6 -activate_license $MODTRAN_PRODUCT_KEY
+
 cp /modtran_runner/test-input-data.json /tests/
-/modtran6/bin/linux/mod6c_cons -version
-/modtran6/bin/linux/mod6c_cons -activate_license $MODTRAN_PRODUCT_KEY
 cd /tests/
-/modtran6/bin/linux/mod6c_cons test-input-data.json
-echo "modtran runner ending"
+$MOD6 test-input-data.json
