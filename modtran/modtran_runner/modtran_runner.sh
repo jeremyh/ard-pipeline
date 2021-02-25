7eee6a424c68c5900f1f145e27a545f0484a74cf
Imam Alam <imam.alam@gmail.com>
2021-02-25 00:19:04 +0000

    scaling-proof modtran runner

--- modtran/modtran_runner/modtran_runner.sh
+++ modtran/modtran_runner/modtran_runner.sh
@@ -3,6 +3,8 @@ export MOD6=/modtran6/bin/linux/mod6c_cons
 $MOD6 -version
 $MOD6 -activate_license $MODTRAN_PRODUCT_KEY
 
-cp /modtran_runner/test-input-data.json /tests/
-cd /tests/
+export OUTPUT_DIR=/tests/$HOSTNAME
+mkdir -p $OUTPUT_DIR
+cp /modtran_runner/test-input-data.json $OUTPUT_DIR
+cd $OUTPUT_DIR
 $MOD6 test-input-data.json
