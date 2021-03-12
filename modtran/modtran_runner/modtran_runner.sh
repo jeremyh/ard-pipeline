50891294cb639016cb59b6036f9325932e9ce4ed
Imam Alam <imam.alam@gmail.com>
2021-03-12 11:59:47 +0000

    k8s job + upload to s3 if possible

--- modtran/modtran_runner/modtran_runner.sh
+++ modtran/modtran_runner/modtran_runner.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
+set -euo pipefail
 export MOD6=/modtran6/MODTRAN6.0/bin/linux/mod6c_cons
+export UUID=$(cat /proc/sys/kernel/random/uuid)
 $MOD6 -version
 $MOD6 -activate_license $MODTRAN_PRODUCT_KEY
 
-export OUTPUT_DIR=/tests/$HOSTNAME
+export OUTPUT_DIR=/tests/$UUID
 mkdir -p $OUTPUT_DIR
 cp /modtran_runner/landsat8_vsir.flt /tests/
 cp /modtran_runner/test2-input-data.json $OUTPUT_DIR
 cd $OUTPUT_DIR
 $MOD6 test2-input-data.json
+if [ -v AWS_ACCESS_KEY_ID ]; then
+    aws s3 sync . s3://dea-dev-eks-nrt-scene-cache/$UUID
+fi
