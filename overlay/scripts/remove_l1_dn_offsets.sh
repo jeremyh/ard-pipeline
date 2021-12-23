c268731e269f75b4c7cd4858666bab4275b3c0c7
Damien Ayers <damien@omad.net>
2021-12-23 12:29:34 +1100

    Add preprocessing script for new S2 L1 data format
    ESA is making a major update to the Sentinel 2 Product format, which
    requires changes to our wagl processing pipeline. In particular, the
    DN (Digital Number) values of the radiometric bands will now be Offset
    by a per band value.
    
    See section 6 of the below notice for details.
    
    https://sentinels.copernicus.eu/web/sentinel/missions/sentinel-2/news/-/asset_publisher/Ac0d/content/id/4712636?_com_liferay_asset_publisher_web_portlet_AssetPublisherPortlet_INSTANCE_Ac0d_redirect=https%3A%2F%2Fsentinels.copernicus.eu%2Fweb%2Fsentinel%2Fmissions%2Fsentinel-2%2Fnews%3Fp_p_id%3Dcom_liferay_asset_publisher_web_portlet_AssetPublisherPortlet_INSTANCE_Ac0d%26p_p_lifecycle%3D0%26p_p_state%3Dnormal%26p_p_mode%3Dview%26_com_liferay_asset_publisher_web_portlet_AssetPublisherPortlet_INSTANCE_Ac0d_cur%3D0%26p_r_p_resetCur%3Dfalse%26_com_liferay_asset_publisher_web_portlet_AssetPublisherPortlet_INSTANCE_Ac0d_assetEntryId%3D4712636

---
+++ overlay/scripts/remove_l1_dn_offsets.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+set -euo pipefail
+
+# Run either in the current directory, or within to the supplied directory
+if [[ -v 1 ]]; then
+    cd "$1"
+fi
+
+if [[ ! -r MTD_MSIL1C.xml ]]; then
+    echo "Unable to find MTD_MSIL1C.xml, please supply a Sentinel 2 Level 1 dataset dir."
+    exit 1
+fi
+
+if ! grep -q Radiometric_Offset_List MTD_MSIL1C.xml; then
+    echo "No Radiometric Offsets found, SKIPPING."
+    exit
+fi
+
+
+# User xmlstarlet to extract the filenames and data offsets
+# shellcheck disable=SC2016
+for op in $(xmlstarlet sel -t -m "//IMAGE_FILE" --var "band_id=position()-1" \
+    --if '//RADIO_ADD_OFFSET[@band_id=$band_id]' \
+    -v "." -o "," -v "//RADIO_ADD_OFFSET[@band_id=\$band_id]" -nl MTD_MSIL1C.xml); do
+    
+    fname=${op%,*}
+    offset=${op#*,}
+
+    echo "Applying Pixel Data Offset to ${fname}.jp2"
+
+    echo Running gdal_calc.py -A "${fname}.jp2" --outfile=temp.tif --calc="numpy.where(A == 0, 0, A+${offset})"
+    gdal_calc.py -A "${fname}.jp2" --outfile=temp.tif --calc="numpy.where(A == 0, 0, A+${offset})"
+    echo Running gdal_translate -co QUALITY=100 -co REVERSIBLE=YES -co YCBCR420=NO temp.tif "${fname}.jp2"
+    gdal_translate -co QUALITY=100 -co REVERSIBLE=YES -co YCBCR420=NO temp.tif "${fname}.jp2"
+    rm -f temp.tif
+done
+
+# Use xmlstarlet again to delete the Radiometric Offset section
+cp MTD_MSIL1C.xml MTD_MSIL1C.xml.orig
+xmlstarlet ed --pf -d "//Radiometric_Offset_List" MTD_MSIL1C.xml.orig > MTD_MSIL1C.xml
