c7a6718c38de974eeec1d9b18ded12309a6ac78e
Imam Alam <imam.alam@gmail.com>
2022-01-19 08:09:19 +0000

    offset correction for provisionals

--- scripts/lib.sh
+++ scripts/lib.sh
@@ -64,6 +64,16 @@ function fetch_sentinel2_granule {
     fi
     log_message $LOG_INFO "Metadata synched"
 
+    # top level metadata if there
+    aws s3 cp --only-show-errors "${DATASTRIP_URL%/datastrip/0}/metadata.xml" "$WORKDIR/$TASK_UUID/l1c-metadata.xml"
+    if [ "$?" -ne 0 ]; then
+        log_message $LOG_ERROR "Unable to fetch top-level metadata, skipping";
+    else
+        # If data includes radiation offsets, remove them and their record in the MTD xml
+        log_message $LOG_INFO "Rewriting Data with radiation offsets (if required)"
+        /scripts/remove_l1_dn_offsets.sh "$WORKDIR/$TASK_UUID"
+    fi
+
     find "$WORKDIR/$TASK_UUID" -type f
     log_message $LOG_INFO "Fetching scene completed"
 }
