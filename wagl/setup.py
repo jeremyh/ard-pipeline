c72d6d47729c792b1ae23dfd29a217e3dbd590c9
Jeremy Hooke <jez@stulk.com>
2023-07-21 16:44:40 +1000

    Initial meson sub file

--- wagl/setup.py
+++ wagl/setup.py
@@ -10,12 +10,9 @@ def configuration(parent_package="", top_path=None):
 
     from numpy.distutils.misc_util import Configuration
 
-    config = Configuration("wagl", parent_package, top_path)
-    config.add_subpackage("scripts")
-    config.add_data_files("acquisition/sensors.json")
-    config.add_data_dir("spectral_response")
-    config.add_extension(
-        "__cast_shadow_mask",
+
+
+    _cast_shadow_mask = py3.extension_module("_cast_shadow_mask",
         [
             "f90_sources/sys_variables.f90",
             "f90_sources/cast_shadow_main.f90",
@@ -24,49 +21,61 @@ def configuration(parent_package="", top_path=None):
             "f90_sources/terrain_occlusion.f90",
             "f90_sources/geo2metres_pixel_size.f90",
         ],
+        install: true,
+        link_language: 'fortran',
+        subdir: 'wagl'
     ),
-    config.add_extension(
-        "__exiting_angle",
+    _exiting_angle = py3.extension_module("_exiting_angle",
         [
             "f90_sources/sys_variables.f90",
             "f90_sources/exiting_angle.f90",
             "f90_sources/earth_rotation.f90",
         ],
+                                          install: true,
+    link_language: 'fortran',
+    subdir: 'wagl'
     ),
-    config.add_extension(
-        "__incident_angle",
+    _incident_angle = py3.extension_module("_incident_angle",
         [
             "f90_sources/incident_angle.f90",
             "f90_sources/earth_rotation.f90",
         ],
+                                           install: true,
+    link_language: 'fortran',
+    subdir: 'wagl'
     ),
-    config.add_extension(
-        "__slope_aspect",
+    _slope_aspect = py3.extension_module("_slope_aspect",
         [
             "f90_sources/sys_variables.f90",
             "f90_sources/slope_aspect.f90",
             "f90_sources/geo2metres_pixel_size.f90",
         ],
+                                         install: true,
+    link_language: 'fortran',
+    subdir: 'wagl'
     ),
-    config.add_extension(
-        "__surface_reflectance",
+    _surface_reflectance = py3.extension_module("_surface_reflectance",
         [
             "f90_sources/surface_reflectance.f90",
             "f90_sources/white_sky.f90",
             "f90_sources/black_sky.f90",
             "f90_sources/brdf_shape.f90",
         ],
+                                                install: true,
+    link_language: 'fortran',
+    subdir: 'wagl'
     ),
-    config.add_extension(
-        "__satellite_model",
+    _satellite_model = py3.extension_module("_satellite_model",
         [
             "f90_sources/sys_variables.f90",
             "f90_sources/geo2metres_pixel_size.f90",
             "f90_sources/satellite_model.f90",
         ],
+                                            install: true,
+    link_language: 'fortran',
+    subdir: 'wagl'
     ),
-    config.add_extension(
-        "__track_time_info",
+    _track_time_info = py3.extension_module("_track_time_info",
         [
             "f90_sources/sys_variables.f90",
             "f90_sources/geod2geo.f90",
@@ -75,9 +84,11 @@ def configuration(parent_package="", top_path=None):
             "f90_sources/satellite_track.f90",
             "f90_sources/track_time_info.f90",
         ],
+                                            install: true,
+    link_language: 'fortran',
+    subdir: 'wagl'
     ),
-    config.add_extension(
-        "__sat_sol_angles",
+    _sat_sol_angles = py3.extension_module("_sat_sol_angles",
         [
             "f90_sources/sys_variables.f90",
             "f90_sources/solar_angle.f90",
@@ -86,12 +97,17 @@ def configuration(parent_package="", top_path=None):
             "f90_sources/compute_angles.f90",
             "f90_sources/satellite_solar_angles_main.f90",
         ],
+                                           install: true,
+    link_language: 'fortran',
+    subdir: 'wagl'
     ),
-    config.add_extension(
-        "__bilinear_interpolation",
+    _bilinear_interpolation = py3.extension_module("_bilinear_interpolation",
         [
             "f90_sources/bilinear_interpolation.f90",
         ],
+                                                   install: true,
+    link_language: 'fortran',
+    subdir: 'wagl'
     )
 
     return config
