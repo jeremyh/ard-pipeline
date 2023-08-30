f8dfd153e041a614faa0852a8d1bfbc9e4419c3b
Jeremy Hooke <jez@stulk.com>
2023-08-30 11:37:52 +1000

    Remove old module build tools

--- deployment/nci/templates/cleanup.bash
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-chmod u+w "{{ target_modules_repo }}/modulefiles/ard-pipeline/{{ target_version }}"
-rm -rf "{{ target_modules_repo }}/modulefiles/ard-pipeline/{{ target_version }}"
-chmod -R u+w "{{ target_modules_repo }}/ard-pipeline/{{ target_version }}"
-rm -rf "{{ target_modules_repo }}/ard-pipeline/{{ target_version }}"
-rm -rf ./build
