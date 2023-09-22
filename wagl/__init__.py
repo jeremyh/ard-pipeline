e841eb271a1bf5050394ca6365abb4302ecb8cc8
Jeremy Hooke <jez@stulk.com>
2023-09-22 11:24:39 +1000

    Consistent imports

--- wagl/__init__.py
+++ wagl/__init__.py
@@ -2,8 +2,8 @@
 # For all usage of h5py.
 import hdf5plugin  # noqa: F401
 
-from . import _version as version_module
+from ._version import __version__
 
-__version__ = _version = version_module.__version__
+_version = __version__
 
 __all__ = ("__version__", "_version")
