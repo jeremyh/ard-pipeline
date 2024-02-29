1a0610bb7f0ea3eafdcd834a6ece18bf5d95ad7c
Imam Tashdid ul Alam <imam.alam@gmail.com>
2024-02-29 22:56:33 +1100

    trig caching for incident/exiting angles

--- wagl/f90_sources/earth_rotation.f90
+++ wagl/f90_sources/earth_rotation.f90
@@ -1,8 +1,13 @@
 SUBROUTINE cal_pole(theta, phi, theta_p, phi_p, thp, php)
 ! does some funky stuff to calculate various angles
-    real theta, phi, theta_p, phi_p, thp, php, offset
+    real, intent(in) :: theta, phi, theta_p, phi_p
+    real, intent(inout) :: thp, php
+    real offset
     real pdiff, costhp, sinphp, cosphp, tnum, tden
-    real eps, pi, d2r
+    real cos_theta, sin_theta, cos_theta_p, sin_theta_p
+    real cos_pdiff, sin_pdiff
+    real sin_thp
+    real eps
 
     eps=1.0e-6
     pi=4.0*atan(1.0)
@@ -14,9 +19,17 @@ SUBROUTINE cal_pole(theta, phi, theta_p, phi_p, thp, php)
         return
     endif
 
-    offset = atan(tan(pi-d2r*phi_p)*cos(d2r*theta_p))
+    cos_theta = cos(d2r*theta)
+    sin_theta = sin(d2r*theta)
+    cos_theta_p = cos(d2r*theta_p)
+    sin_theta_p = sin(d2r*theta_p)
+
+    offset = atan(tan(pi-d2r*phi_p)*cos_theta_p)
     pdiff = d2r*(phi-phi_p)
-    costhp = cos(d2r*theta)*cos(d2r*theta_p)+sin(d2r*theta)*sin(d2r*theta_p)*cos(pdiff)
+    cos_pdiff = cos(pdiff)
+    sin_pdiff = sin(pdiff)
+
+    costhp = cos_theta*cos_theta_p+sin_theta*sin_theta_p*cos_pdiff
 
     if (costhp.ge.1.0-eps) then
         thp=0.0
@@ -24,8 +37,9 @@ SUBROUTINE cal_pole(theta, phi, theta_p, phi_p, thp, php)
         return
     else
         thp=acos(costhp)/d2r
-        sinphp=sin(d2r*theta)*sin(pdiff)/sin(d2r*thp)
-        cosphp=(cos(d2r*theta)*sin(d2r*theta_p)-sin(d2r*theta)*cos(d2r*theta_p)*cos(pdiff))/sin(d2r*thp)
+        sin_thp = sin(d2r*thp)
+        sinphp=sin_theta*sin_pdiff/sin_thp
+        cosphp=(cos_theta*sin_theta_p-sin_theta*cos_theta_p*cos_pdiff)/sin_thp
         if (abs(sinphp).le.eps) then
             if (cosphp.gt.eps) then
                 php=0.0-offset/d2r
@@ -43,8 +57,8 @@ SUBROUTINE cal_pole(theta, phi, theta_p, phi_p, thp, php)
         endif
     endif
 
-    tnum=sin(d2r*theta)*sin(pdiff)
-    tden=(cos(d2r*theta)*sin(d2r*theta_p)-sin(d2r*theta)*cos(d2r*theta_p)*cos(pdiff))
+    tnum=sin_theta*sin_pdiff
+    tden=(cos_theta*sin_theta_p-sin_theta*cos_theta_p*cos_pdiff)
     php=(atan2(tnum,tden)-offset)/d2r
     return
 END SUBROUTINE cal_pole
