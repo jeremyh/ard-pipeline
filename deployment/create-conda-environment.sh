3e33dd71e27cf4853ac74539c3bdb5f0b3955131
Jeremy Hooke <jez@stulk.com>
2024-10-23 14:48:24 +1100

    Restore mamba by using miniforge

--- deployment/create-conda-environment.sh
+++ deployment/create-conda-environment.sh
@@ -9,7 +9,7 @@ pip_args=(--no-binary :all:)
 location="conda"
 # If they provided a location argument, install there instead
 if [ $# -gt 0 ]; then
-    location="$1"
+  location="$1"
 fi
 
 mkdir -p "${location}"
@@ -30,17 +30,15 @@ fi
 
 cache_dir=~/.cache
 mkdir -p "${cache_dir}"
-
-conda_file="Miniconda3-py311_23.5.2-0-${osname}-${archname}.sh"
+conda_file="Miniforge3-${osname}-${archname}.sh"
 conda_inst="${cache_dir}/${conda_file}"
 
 if [ ! -f "${conda_inst}" ]; then
   wget --progress=dot:giga \
-      -O "${conda_inst}" \
-      "https://repo.anaconda.com/miniconda/${conda_file}"
+    -O "${conda_inst}" \
+    "https://github.com/conda-forge/miniforge/releases/latest/download/${conda_file}"
   chmod +x "${conda_inst}"
 fi
-
 
 "${conda_inst}" -b -f -p "${location}"
 set +ux
@@ -49,20 +47,20 @@ set +ux
 . "${location}/bin/activate"
 
 conda install -c conda-forge -n base mamba
-conda env create -n base -f "$(dirname "$0")/environment.yaml"
+mamba env update -n base -f "$(dirname "$0")/environment.yaml"
 
 # Freeze the environment as it exists without our locally-installed  packages.
 # conda env export --from-history  > "${location}/environment.yaml"
 
 # These version defaults may be overidden by setting them before calling the script.
 pip install "${pip_args[@]}" \
-    "git+https://github.com/ubarsc/rios@rios-${rios_version:-1.4.10}#egg=rios" \
-    "git+https://github.com/ubarsc/python-fmask@pythonfmask-${fmask_version:-0.5.7}#egg=python-fmask" \
-    awscli boto boto3
+  "git+https://github.com/ubarsc/rios@rios-${rios_version:-1.4.10}#egg=rios" \
+  "git+https://github.com/ubarsc/python-fmask@pythonfmask-${fmask_version:-0.5.7}#egg=python-fmask" \
+  awscli boto boto3
 
 if [ "$clean_all" = true ]; then
-    conda clean --all -y
-    rm "${conda_inst}"
+  conda clean --all -y
+  rm "${conda_inst}"
 fi
 
 echo
